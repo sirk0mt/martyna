7949af26877b7a8043d93e02c7b15b6d8e1a564c
sirk0mt <129403238+sirk0mt@users.noreply.github.com>
2024-05-22 19:42:14 +0200

    Grubsza aktualizacja
    - drobne poprawki
    - zmiana obsługi odebranych z czujnika pakietów
    - rozdzielenie obsługi pakietów na funkcje
    - dodanie CLI dla PC

--- nowy.c
+++ nowy.c
@@ -8,11 +8,12 @@ uint8_t     temp1_offset        = 4;                    // offset w pakiecie dla
 uint8_t     temp2_offset        = temp1_offset + 4;     // offset w pakiecie dla 2 temperatury
 uint8_t     temp3_offset        = temp2_offset + 4;     // offset w pakiecie dla 3 temperatury
 uint8_t     time_offset         = temp3_offset + 4;     // offset w pakiecie dla czasu od startu czujnika
-int         packet_byte_index   = 0;                    // index odebranego znaku pakietu
+int         received_byte_index   = 0;                  // index odebranego znaku pakietu
 int         incomplete_packets  = 0;                    // "urwane" pakiety
 int         broken_packets      = 0;                    // uszkodzone pakiety
-char        incoming_byte;                              // odebrany bajt
+int         received_packets    = 0;                    // ilość odebranych poprawnych pakietów
 float       temp1, temp2, temp3, received_time;         // zmienne dla odebranych liczb zmiennoprzecinkowych
+String      pc_input;                                   // zmienna dla danych wejściowych z PC
 
 union byte_to_float_t {                                 // jednak unia :) - pozwala ona na łatwiejsze manipulowanie danymi w tym samym obszarze pamięci
     uint8_t bytes[4];                                   // tablica bajtowa dla kolejnych elementów liczby
@@ -44,6 +45,19 @@ uint16_t calc_checksum() {
 }
 
 /**
+ * \brief               Funkcja licząca procenty
+ * \param[numerator]    Licznik
+ * \param[denominator]  Mianownik
+ * \return              Obliczony procent
+*/
+float calc_percentage(int numerator, int denominator) {
+  if (denominator == 0) {     // Sprawdź czy nie dzielimy przez 0
+    return 0;
+  }
+  return ((float)numerator / (float)denominator) * 100; // zwrócenie obliczonego procentu
+}
+
+/**
  * \brief       Funkcja służąca do parsowania zmiennej typu uint16_t z 2 zmiennych typu uint8_t
  * \param[msb]  Najbardziej znaczący bajt
  * \param[lsb]  Mniej znaczący bajt
@@ -57,10 +71,106 @@ uint16_t create_uint16(uint8_t msb, uint8_t lsb) {
  * \brief     Funkcja do obsługi całego poprawnie odebranego pakietu danych (znów można by się pokusić o przekazanie wskaźnika i sługości tablicy jako parametrów zamiast praca na globalach)
 */
 void handle_whole_packet() {
-  temp1 = parse_float(temp1_offset);        // przeliczenie temperatury z tablicy odebranego pakietu od zadanego offsetu
-  temp2 = parse_float(temp2_offset);
-  temp3 = parse_float(temp3_offset);
-  received_time = parse_float(time_offset); // przeliczenie czasu od włączenia czujnika z tablicy odebranego pakietu od zadanego offsetu
+  if (calc_checksum() == create_uint16(packet[20], packet[21])) { // jeśli obliczona suma kontrolna zgadza się z odebraną sumą
+    temp1 = parse_float(temp1_offset);                            // przeliczenie temperatury z tablicy odebranego pakietu od zadanego offsetu
+    temp2 = parse_float(temp2_offset);
+    temp3 = parse_float(temp3_offset);
+    received_time = parse_float(time_offset);                     // przeliczenie czasu od włączenia czujnika z tablicy odebranego pakietu od zadanego offsetu                                 
+    Serial.println("Czas pracy czujnika: " + String(received_time) +
+                    " TEMP1: " + String(temp1) +
+                    " TEMP2: " + String(temp2) +
+                    " TEMP3: " + String(temp3));
+    // ToDo: Zapisz odebrane dane na karcie
+
+    received_packets++;
+  } else {                                                        // jeśli odebrana suma kontrolna różni się od obliczonej
+    broken_packets++;
+    Serial.println("BŁĄD - Suma kontrolna pakietu różni się od obliczonej sumy. Liczba uszkodzonych pakietów: " + String(broken_packets));
+    Serial.println("INFO - Procent poprawnie odebranych pakietów: " +
+                    String(calc_percentage((incomplete_packets + broken_packets), received_packets)) + "%");
+  }
+}
+
+/**
+ * \brief                 Funkcja obsługująca odebrane pakiety z czujnika
+ * \param[received_byte]  Odebrany z czujnika bajt danych
+*/
+void handle_received_packet(uint8_t received_byte) {
+  if ((received_byte == packet_header[0]) && (received_byte_index != 0)) {  // jeśli odebrany znak to pierwszy znak nagłówka pakietu ale nie jest pierwszym bajtem pakietu        
+    received_byte_index = 0;                // zresetuj index odbieranego bajtu
+    incomplete_packets++;                 // podnieś ilość urwanych pakietów
+    Serial.println("BŁĄD - Urwano ostatni pakiet danych. Liczba niekompletnych pakietów: " + String(incomplete_packets));
+    Serial.println("INFO - Procent poprawnie odebranych pakietów: " +
+                    String(calc_percentage((incomplete_packets + broken_packets), received_packets)) + "%");
+  }
+  
+  if (received_byte_index < packet_header_size) {                     // jeśli index odbieranego pakietu jest mniejszy niż wielkość nagłówka
+    if (received_byte == packet_header[received_byte_index]) {        // jeśli odebrany bajt zgadza się z kolejnymi bajtami nagłówka
+      packet[received_byte_index++] = received_byte;                  // wpisanie odebranego bajtu do tablicy pakietu
+    } else {                                                          // jeśli odebrany bajt nie zgadza się z kolejnymi bajtami nagłówka
+      received_byte_index = 0;                                        // zresetuj index odbieranego bajtu
+      incomplete_packets++;                                           // podnieś ilość urwanych pakietów
+      Serial.println("BŁĄD - Urwano ostatni pakiet danych. Liczba niekompletnych pakietów: " + String(incomplete_packets));
+      Serial.println("INFO - Procent poprawnie odebranych pakietów: " +
+                    String(calc_percentage((incomplete_packets + broken_packets), received_packets)) + "%");
+    }
+  } else {                                                            // jeśli index odbieranego minął wielkość nagłówka
+    packet[received_byte_index++] = received_byte;                    // przypisanie odebranego bajtu do kolejnego elementu tablicy pakietu
+    
+    if (received_byte_index == packet_size) {                         // jeśli odebrano pełny pakietu
+      handle_whole_packet();                                          // obsłuż cały poprawnie odebrany pakiet
+      received_byte_index = 0;                                        // wyzeruj index odebranego pakietu
+    }
+  }
+}
+
+/**
+ * \brief     Funkcja wyświetlająca pomoc
+*/
+void print_help() {
+  Serial.println("Dostępne opcje:");
+  Serial.println("-> reset - zeruje liczniki odebranych i błędnych pakietów");
+  Serial.println("-> info - wyświetlenie stanu liczników odebranych pakietów");
+  // ToDo
+}
+
+/**
+ * \brief     Funkcja resetująca liczniki odebranych i błędnych pakietów
+*/
+void handle_reset() {
+  incomplete_packets = 0;
+  broken_packets = 0;
+  received_packets = 0;
+  Serial.println("INFO: Liczniki zostały wyzerowane");
+}
+
+/**
+ * \brief     Funkcja wyświetlająca stan liczników odebranych pakietów
+*/
+void handle_info() {
+  Serial.println("INFO: Poprawne pakiety - " + String(received_packets) +
+                  " Urwane pakiety: " + String(incomplete_packets) +
+                  " Uszkodzone pakiety: " + String(broken_packets) +
+                  " Procent poprawnych pakietów: " + String(calc_percentage(
+                                                            (incomplete_packets + broken_packets),
+                                                              received_packets)) + "%");
+}
+
+
+/**
+ * \brief     Funkcja do obsługi odebranego z PC polecenia
+*/
+void handle_pc_command() {
+  if (pc_input == "help") {
+    print_help();
+  } else if (pc_input == "reset") {
+    handle_reset();
+  } else if (pc_input == "info") {
+    handle_info();
+  } else {
+    Serial.println("Nieznana komenda. Wpisz 'help' aby wyświetlić pomoc.");
+  }
+  pc_input = "";
 }
 
 /**
@@ -69,35 +179,27 @@ void handle_whole_packet() {
 void setup() {
   Serial.begin(9600);                     // inicjalizacja portu do komunikacji z PC
   Serial1.begin(9600);                    // inicjalizacja portu do komunikacji z czujnikiem temp
+
+  Serial.println();
+  Serial.println("-----------------------------------------------");
+  Serial.println("Rejestrator temperatury został zainicjalizowany");  // debug info wysłane to terminala PC
+  Serial.println("-----------------------------------------------");
+  print_help();
+  Serial.println("-----------------------------------------------");
 }
 
 /**
  * \brief     Główna pętla programu (ta funkcja wykonuje się w nieskończoność)
 */
 void loop() {
-  if (Serial1.available()) {              // jeśli dostępny jest bajt na porcie szeregowym
-    incoming_byte = Serial.read();        // przypisanie odebranego bajtu do zmiennej
-    
-    if (packet_byte_index < packet_header_size) {                       // jeśli index odbieranego pakietu jest mniejszy niż wielkość nagłówka
-      if (incoming_byte == packet_header[packet_byte_index]) {          // jeśli odebrany bajt zgadza się z kolejnymi bajtami nagłówka
-        packet[packet_byte_index++] = incoming_byte;                    // wpisanie odebranego bajtu do tablicy pakietu
-      } else {                                                          // jeśli odebrany bajt nie zgadza się z kolejnymi bajtami nagłówka
-        packet_byte_index = 0;                                          // zresetuj index odbieranego bajtu
-        incomplete_packets++;                                           // podnieś ilość urwanych pakietów
-      }
-    } else {                                                            // jeśli index odbieranego minął wielkość nagłówka
-      packet[packet_byte_index++] = incoming_byte;                      // przypisanie odebranego bajtu do kolejnego elementu tablicy pakietu
-      
-      if (packet_byte_index == packet_size) {                           // jeśli odebrano pełny pakietu
-        if (calc_checksum() == create_uint16(packet[20], packet[21])) { // jeśli obliczona suma kontrolna zgadza się z odebraną sumą
-          handle_whole_packet();                                         
-        } else {                                                        // jeśli odebrana suma kontrolna różni się od obliczonej
-          broken_packets++;
-        }
-
-        packet_byte_index = 0;                                          // wyzeruj index odebranego pakietu
-      }
-    }
+  if (Serial1.available()) {                // jeśli dostępny jest bajt na porcie szeregowym czujnika
+    handle_received_packet(Serial1.read()); // obsłuż odebrany bajt danych
   }
-  delay(80);                                                            // nwm czemu akurat 80ms, ale tak było w kodzie
+
+  if (Serial.available()) {                   // jeśli dostępny jest bajt na porcie szeregowym czujnika
+    pc_input = Serial.readStringUntil('\n');  // odbierz z portu szeregoweg cały ciąg aż do znaku nowej linii
+    pc_input.trim();                          // wyczyść ciąg z białych znaków na jego końcu
+    handle_pc_command();                      // obsłóż odebrane z PC polecenie
+  }
+  delay(80);                                  // nwm czemu akurat 80ms, ale tak było w kodzie
 }
